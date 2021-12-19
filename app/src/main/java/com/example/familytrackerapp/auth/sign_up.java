55fd539441005ecc3e6fc24da8d4f6324855389b
Shubham Vig <shubhamgithub@outlook.com>
2021-12-19 13:35:10 +0530

    authentication ui completed

--- app/src/main/java/com/example/familytrackerapp/auth/sign_up.java
+++ app/src/main/java/com/example/familytrackerapp/auth/sign_up.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -23,6 +24,8 @@ public class sign_up extends AppCompatActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_login);
         t_user=findViewById(R.id.user);
         t_pass=findViewById(R.id.pass);
