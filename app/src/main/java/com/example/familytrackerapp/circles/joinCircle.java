7947b868f211fc325c335d471d4d14047d302c40
Ram-20062003 <ramajs2003@gmail.com>
2022-01-28 02:24:02 +0530

    feat: Join circle feature added

---
+++ app/src/main/java/com/example/familytrackerapp/circles/joinCircle.java
@@ -0,0 +1,129 @@
+package com.example.familytrackerapp.circles;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.familytrackerapp.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class joinCircle extends AppCompatActivity {
+    Button b_join;
+    EditText joinText;
+    int t=1;
+    private static final String TAG="join";
+    FirebaseFirestore firebaseFirestore=FirebaseFirestore.getInstance();
+    FirebaseAuth firebaseAuth= FirebaseAuth.getInstance();
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+        List<String> list=new ArrayList<>();
+        List<String> reverselist=new ArrayList<>();
+
+
+        Map<String,List<String>> map=new HashMap<String,List<String>>();
+        Map<String,List<String>> reverseMap=new HashMap<String,List<String>>();
+        Map<String,String> map2= new HashMap<>();
+        b_join=findViewById(R.id.createCirclebutton);
+        joinText=findViewById(R.id.joinText);
+        firebaseFirestore.collection("groups").document(firebaseAuth.getUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if(documentSnapshot.exists())
+                {
+                    list.addAll((Collection<? extends String>) documentSnapshot.get("Group"));
+                    Log.d(TAG, "onSuccess: "+documentSnapshot.get("Group"));
+                }
+                else
+                {
+                }
+            }
+        });
+        b_join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                t++;
+                String join=joinText.getText().toString();
+                list.add(join);
+                map.put("Group",list);
+                firebaseFirestore.collection("groups").document(firebaseAuth.getUid().toString()).set(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Log.d(TAG, "Success: Added successfully");
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d(TAG, "onFailure: "+e);
+
+                    }
+                });
+                Log.d(TAG, "join: "+join);
+
+                firebaseFirestore.collection("CheckGroup").document(join).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if(documentSnapshot.exists() && t ==2 )
+                        {
+                            reverselist.addAll((Collection<? extends String>) documentSnapshot.get("id"));
+                            reverselist.add(firebaseAuth.getUid());
+                            reverseMap.put("id",reverselist);
+                            Log.d(TAG, "join: "+join);
+                            firebaseFirestore.collection("CheckGroup").document(join).set(reverseMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Log.d(TAG, "Success: Added successfully");
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG, "onFailure: "+e);
+
+                                }
+                            });
+                        }
+                        else
+                        {
+                            reverselist.add(firebaseAuth.getUid());
+                            reverseMap.put("id",reverselist);
+                            Log.d(TAG, "join: "+join);
+                            firebaseFirestore.collection("CheckGroup").document(join).set(reverseMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Log.d(TAG, "Success: Added successfully");
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG, "onFailure: "+e);
+
+                                }
+                            });
+                        }
+                    }
+                });
+
+
+
+            }
+        });
+    }
+}
