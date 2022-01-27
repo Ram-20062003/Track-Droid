a337a30d173826c4a88fa8b34790c17c0cf16027
Ram-20062003 <ramajs2003@gmail.com>
2022-01-28 02:25:33 +0530

    feat: chat view UI

---
+++ app/src/main/java/com/example/familytrackerapp/chat/chat.java
@@ -0,0 +1,17 @@
+package com.example.familytrackerapp.chat;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.familytrackerapp.R;
+
+public class chat extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+    }
+}
