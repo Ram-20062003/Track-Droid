57ccc87744a9ade0e7f3228d95a51f95bc23ab43
Ram-20062003 <ramajs2003@gmail.com>
2022-01-28 02:23:10 +0530

    feat: added Create Circle feature

---
+++ app/src/main/java/com/example/familytrackerapp/circles/createCircle.java
@@ -0,0 +1,17 @@
+package com.example.familytrackerapp.circles;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.familytrackerapp.R;
+
+public class createCircle extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+
+    }
+}
